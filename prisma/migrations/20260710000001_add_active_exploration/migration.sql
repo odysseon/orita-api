4a3cd80a9b3bfc507bfe4938e9b0d64af42ba33d
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2026-07-10 10:45:38 +0100

    feat(schema): add active exploration fields to User

---
+++ prisma/migrations/20260710000001_add_active_exploration/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN "activeExplorationLat" DOUBLE PRECISION;
+ALTER TABLE "User" ADD COLUMN "activeExplorationLng" DOUBLE PRECISION;
+ALTER TABLE "User" ADD COLUMN "activeExplorationName" TEXT;
