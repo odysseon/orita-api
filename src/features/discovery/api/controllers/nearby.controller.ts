e2da94e58236e71d9cc56eae88189a3f6352d5bd
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2026-07-21 10:09:33 +0100

    fix(discovery): remove controller-level UsePipes to inherit global implicit conversion

--- src/features/discovery/api/controllers/nearby.controller.ts
+++ src/features/discovery/api/controllers/nearby.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { DiscoveryService } from '../../application/services/discovery.service.js';
 import { NearbyQueryDto } from '../dto/nearby-query.dto.js';
 import { ApiTags , ApiBearerAuth } from '@nestjs/swagger';
@@ -10,7 +10,6 @@ export class NearbyController {
   constructor(private readonly discoveryService: DiscoveryService) {}
 
   @Get()
-  @UsePipes(new ValidationPipe({ transform: true }))
   async getNearby(@Query() query: NearbyQueryDto) {
     return this.discoveryService.getNearby(query);
   }
