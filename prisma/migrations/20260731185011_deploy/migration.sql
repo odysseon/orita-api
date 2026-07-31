e47f425ed27a964a49e7a2bad4f1d7875b779c94
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2026-07-31 18:51:27 +0000

    chore: prod migration

---
+++ prisma/migrations/20260731185011_deploy/migration.sql
@@ -0,0 +1,17 @@
+-- DropIndex
+DROP INDEX "business_service_areas_admin_enabled_type_idx";
+
+-- DropIndex
+DROP INDEX "business_service_areas_center_geography_idx";
+
+-- DropIndex
+DROP INDEX "business_service_areas_polygon_geometry_idx";
+
+-- DropIndex
+DROP INDEX "listing_service_areas_admin_enabled_type_idx";
+
+-- DropIndex
+DROP INDEX "listing_service_areas_center_geography_idx";
+
+-- DropIndex
+DROP INDEX "listing_service_areas_polygon_geometry_idx";
